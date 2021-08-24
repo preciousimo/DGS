0f1993095a78e8a68d2a656815e39351855efa7f
preciousimo <preciousimoniakemu@gmail.com>
2021-08-24 23:54:15 +0100

    Adjust the naming convention

--- doggy/views.py
+++ doggy/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'doggy/home.html')
+def index(request):
+    return render(request, 'doggy/index.html')
